65e4ab065b5819cbc93fa8919a496e5597fc9097
Reda El Khattabi <redaea+github@gmail.com>
2012-01-08 22:55:57 +0200

    wm_button.sh: restrict to current viewport

--- gencfs_mount.sh
+++ gencfs_mount.sh
@@ -5,8 +5,10 @@ if [[ -z "`ls $1/.*encfs*.xml`" ]]; then
 	exit 1
 fi
 
-
 TEMP=`mktemp -d`
-encfs  --idle=1 --extpass="zenity --password --title=encfs" "$1" "$TEMP" && xdg-open "$TEMP" || zenity --error --text="Password incorrect"
+encfs  --idle=1 --extpass="zenity --password --title=encfs" "$1" "$TEMP" >&2 &&
+	echo "$TEMP" &&
+	xdg-open "$TEMP" || 
+	zenity --error --text="Password incorrect"
 
 # cd $TEMP; x-terminal-emulator
--- wm_button.sh
+++ wm_button.sh
@@ -5,17 +5,34 @@
 #get current window
 #WIN=`xprop -root | grep _NET_ACTIVE_WINDOW\(WINDOW\) | sed "s/.*# //"`
 
+#viewport data
+VPWID=`xwininfo -root | grep geometry | sed "s/[^0-9]*\([0-9]*\)x\([0-9]*\)+0+0/\1/"`
+VPHGT=`xwininfo -root | grep geometry | sed "s/[^0-9]*\([0-9]*\)x\([0-9]*\)+0+0/\2/"`
+
 #get topmost maximized window
 xprop -root _NET_CLIENT_LIST_STACKING |
   sed "s/.*# //" | sed s"/, /\n/g" | tac | 
-  while read w; do 
-	xprop -id $w | grep _NET_WM_STATE_MAXIMIZED_VERT &&
-# TODO test if window is in current viewport 
-	  case "$1" in
+  while read w; do
+	#window is maximized
+	xprop -id $w | grep _NET_WM_STATE_MAXIMIZED_VERT > /dev/null || continue
+	#window positions relative to current viewport
+	WINX=`xwininfo -id $w | grep "Absolute upper-left X" | sed "s/.*:[^0-9]*//"`
+	WINY=`xwininfo -id $w | grep "Absolute upper-left Y" | sed "s/.*:[^0-9]*//"`
+	WINW=`xwininfo -id $w | grep "Width" | sed "s/.*:[^0-9]*//"`
+        WINH=`xwininfo -id $w | grep "Height" | sed "s/.*:[^0-9]*//"`
+	[[ $(($WINX+($WINW/2))) -gt 0 ]] || continue
+	[[ $(($WINX+($WINW/2))) -lt $VPWID ]] || continue
+	[[ $(($WINY+($WINH/2))) -gt 0 ]] || continue
+	[[ $(($WINY+($WINH/2))) -lt $VPHGT ]] || continue
+	#topmost maximized window found
+	echo $w
+	case "$1" in
 		"close" ) wmctrl -ic $w ;;
 		"min"   ) wmctrl -ir $w -b add,hidden ;; #FIXME
 		"max"   ) wmctrl -ir $w -b toggle,maximized_vert,maximized_horz ;;
 		"unmax" ) wmctrl -ir $w -b remove,maximized_vert,maximized_horz ;;
-	  esac && exit
+	esac
+	#window found, we quit
+	exit
   done
 
